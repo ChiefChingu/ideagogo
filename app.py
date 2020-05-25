4688a708b0533150c6228532f2eb4b550fd9ffe2
ChinguSangho <sangho.grolleman@gmail.com>
2020-05-25 10:46:30 +0200

    Finish update functionality. Basic CRUD functionality done!

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def ideas():
 @app.route("/idea-details/<idea_id>")
 def idea_details(idea_id):
     the_idea = mongo.db.ideas.find_one({"_id": ObjectId(idea_id)})
-    print(the_idea)
     return render_template("idea-details.html", title="Idea details", idea=the_idea)
 
 
@@ -44,43 +43,28 @@ def addidea():
 @app.route("/insertidea", methods=["POST"])
 def insert_idea():
     ideas = mongo.db.ideas
-    # ideas.idea._id = ObjectId()
     x = ideas.insert_one(request.form.to_dict())
-
-    print(x.inserted_id)
-
     return redirect(url_for("idea_details", idea_id=x.inserted_id))
 
-    # result = userTable.insert_one(userDict)
-    # print(result.inserted_id)
-    # print(result.acknowledged)
-    # return redirect(url_for("idea_details", idea_id=idea._id))
-    # _id = userTable.insert(userDict)
-    # print(last_insert_id())
-    # id = request.values.get("_id")
-    # print(property())
-    # print(pymongo.results.InsertOneResult.inserted_id)
-    # print(request.form)
-    return redirect(url_for("ideas"))
 
-    # return redirect(url_for("idea_details", idea_id=idea._id))
-    # print(ideas.find().sort("_id", pymongo.DESCENDING).limit(1))
-    # return redirect(url_for('.do_foo', messages=messages))
-
-    # return render_template("idea-details.html", title="Idea details", idea_id=idea._id)
-    # return redirect(url_for("ideas"))
-    # redirect to idea_details: cannot get it to work...
+@app.route("/edit_idea/<idea_id>")
+def edit_idea(idea_id):
+    the_idea = mongo.db.ideas.find_one({"_id": ObjectId(idea_id)})
+    return render_template("editidea.html", title="Edit Idea", idea=the_idea)
 
 
-# idea_id=idea._id
-# return redirect(url_for("idea-details/<idea_id>", {"_id": ObjectId(idea_id)}))
-# the_idea = mongo.db.ideas.find_one({"_id": ObjectId(idea_id)})
-# return redirect(url_for("idea_details"), title="Idea details", idea=the_idea)
+@app.route("/update_idea/<idea_id>", methods=["POST"])
+def update_idea(idea_id):
+    ideas = mongo.db.ideas
+    ideas.update(
+        {"_id": ObjectId(idea_id)},
+        {
+            "idea_title": request.form.get("idea_title"),
+            "idea_summary": request.form.get("idea_summary"),
+        },
+    )
 
-
-@app.route("/loading")
-def lodaing():
-    return render_template("addidea.html", title="Add Idea")
+    return redirect(url_for("idea_details", idea_id=idea_id))
 
 
 @app.route("/delete_idea/<idea_id>")
