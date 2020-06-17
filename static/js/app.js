1fcaa0040133adc5b61991aae811d23ff8bfdd2b
ChinguSangho <sangho.grolleman@gmail.com>
2020-06-17 13:37:06 +0200

    Change logo and mobile menu. Use own style instead of Materialize.

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,22 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     var elems = document.querySelectorAll('.fixed-action-btn');
-//     var instances = M.FloatingActionButton.init(elems, {
-//         // direction: 'left',
-//         // hoverEnabled: false
-//     });
-// });
+class MobileMenu {
+    constructor() {
+        this.menuIcon = document.querySelector(".site-nav__menu-icon")
+        this.menuContent = document.querySelector(".site-nav__overlay")
+        this.events()
+    }
+
+    events() {
+        this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+    }
+
+    toggleTheMenu() {
+
+        this.menuContent.classList.toggle("site-nav__overlay--is-visible")
+        this.menuIcon.classList.toggle("site-nav__menu-icon--close-x")
+    }
+}
+
+new MobileMenu();
+
+// In case of modules for js. For now only a small file, so one file will do.
+// export default MobileMenu 
