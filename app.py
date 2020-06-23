b535958aa4afe02c5bbe4d847a83dfe329363d61
ChinguSangho <sangho.grolleman@gmail.com>
2020-06-23 12:38:16 +0200

    Remove duplicate statement and keep the right one.

--- app.py
+++ app.py
@@ -468,5 +468,5 @@ def add_tag():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True)
+
